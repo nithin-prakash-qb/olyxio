728595b2c7b28561340f2f2e46712a63a2dca1c9
nithin.prakash <nithin.prakash@qburst.com>
2022-10-12 17:55:04 +0530

    started navigation menu

--- materials/app.js
+++ materials/app.js
@@ -1,30 +1,50 @@
-let array=[]
-let getData = () => {
-    fetch("testimonial.json")
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    array=data
-  });
-}
+const getData = async () => {
+  await fetch("testimonial.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let testimonial = data["testimonial"];
+      testimonial.forEach((testObj) => {
+        let parent = document.getElementById("testimonials");
+        let div1 = document.createElement("div");
+        div1.setAttribute("class", "testi");
+        div1.innerHTML = `<div class="testi_image_designation">
+                                <div>
+                                    <img src="${testObj.image}" alt="" />
+                                </div>
+                                <div class="name_designation_div">
+                                    <h4 class="image_name">${testObj.name}</h4>
+                                    <p class="image_designation">${testObj.designation}</p>
+                                </div>
+                            </div>
+                            <p class="test_para">
+                                ${testObj.description}
+                            </p>
+                          </div>`;
+        parent.appendChild(div1);
+      });
+    });
+
+};
 getData()
 
-console.log(array);
-// Create element:
-const para = document.createElement("h4");
-para.innerHTML = "Sifat Hasannn";
+// let parent = document.getElementById("testimonials");
+// let div1 = document.createElement("div");
+// div1.setAttribute("class", "testi");
+// let div2 = document.createElement("div");
+// div2.setAttribute("class", "testi_image_designation");
+// div2.innerHTML = `<div> <img src="" alt="" /> </div>
+//                 <h4 class="image_name"></h4>
+//                 <p class="image_designation"></p>`;
+// parent.appendChild(div1);
+// div1.appendChild(div2)
 
-// Append to another element:
-document.getElementsByClassName("image_name").appendChild(para);
+// Hamberger
+hamMenu = document.getElementById('ham_menu')
+hamMenu.addEventListener("click",()=>{
+    let div1 = document.createElement("div");
+    
+    
+});
 
-let parent = document.getElementById('testimonials')
-let div1=document.createElement('div')
-let div2=document.createElement('div')
-div1.innerHTML=`<div>
-<img src="${obj[0].image}" alt="" />
-</div>`
-div2.innerHTML=`<h4 class="image_name">${name}</h4>
-<p class="image_designation">${desig}</p>`
-parent.appendChild(div1)
-parent.appendChild(div2)
