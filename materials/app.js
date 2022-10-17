a5a06e7601719565e0077a7002c045287b03e70c
nithin.prakash <nithin.prakash@qburst.com>
2022-10-17 17:36:05 +0530

    opening and closing of contents of the side menu bar completed

--- materials/app.js
+++ materials/app.js
@@ -1,5 +1,5 @@
-const getData = async () => {
-  await fetch("testimonial.json")
+const getData = () => {
+  fetch("testimonial.json")
     .then((res) => {
       return res.json();
     })
@@ -36,54 +36,100 @@ const getData = async () => {
       });
 
       // Loading Main li
+
       let sideMenu = data["sideMenu"];
-      sideMenu.forEach((testObj,index) => {
+      sideMenu.forEach((testObj, index) => {
         let parent = document.getElementById("main_u_li");
         let pop_up = document.createElement("li");
         pop_up.setAttribute("class", "main_li");
 
         let icon = document.createElement("i");
-        icon.setAttribute("class", "fa-solid fa-circle-chevron-down");
-        icon.setAttribute("id", `${testObj.mainmenu}`);
+        icon.setAttribute(
+          "class",
+          "fa-solid fa-circle-chevron-down arrowDownIcon"
+        );
+        // icon.setAttribute("id", `${testObj.mainmenu}`);
+        // icon.setAttribute("class","arrowDownIcon");
 
-        let listContainer = document.createElement('ul')
-        listContainer.className = 'sub_li'
+        let listContainer = document.createElement("ul");
+        listContainer.className = "sub_li hiddens";
+        listContainer.dataset.menuId = testObj.mainmenu
 
-        // console.log(listContainer.innerHTML.trim())
+        let anchorTag = document.createElement("a");
+        anchorTag.className = "myAnchor";
 
-        icon.addEventListener("click", () => {
-            testObj["subMenu"].forEach(x => {
-            let listItemChild = document.createElement("li")
-            listItemChild.innerHTML=`${Object.keys(x)[0]}`
-            listContainer.appendChild(listItemChild)
-            // document.getElementById(id).innerHTM
-            if(document.getElementsByClassName('sub_li')[0].innerHTML.trim() == ""){
-              listContainer.appendChild(listItemChild)
-            }else{
-              
-            }
-            
-          })
+
+        // loading subli for each main li
+        // anchorTag.addEventListener("click", (event) => {
+        //   console.log(event.target);
+        //   let dropDown = anchorTag.nextElementSibling;
+          // if (Object.values(dropDown.classList).includes("hiddens")) {
+          //   dropDown.classList.remove("hiddens");
+          // } else {
+          //   dropDown.classList.add("hiddens");
+          // }
+        // });
+
+        // Trying out 
+        anchorTag.addEventListener("click", (event) => {
+          // console.log(event.target);
+          let clicked = event.target
+          console.log(event.target)
+          let dropDown = clicked.nextElementSibling;
+          console.log(dropDown)
+          if (Object.values(dropDown.classList).includes("hiddens")) {
+            dropDown.classList.remove("hiddens");
+          } else {
+            dropDown.classList.add("hiddens");
+          }
+
+          // let all_anchor_tag=document.querySelectorAll(".myAnchor")
+         
+          // all_anchor_tag.forEach((y)=>{
+          //   let dropDown = y.nextElementSibling;
+          //   if (Object.values(dropDown.classList).includes("hiddens")) {
+          //     dropDown.classList.remove("hiddens");
+          //   }else{
+          //     dropDown.classList.add("hiddens");
+          //   }
+
+          //   }
+          // )
+          // let dropDown = anchorTag.nextElementSibling;
+
+          // if (Object.values(dropDown.classList).includes("hiddens")) {
+          //   dropDown.classList.remove("hiddens");
+          // } else {
+          //   dropDown.classList.add("hiddens");
+          // }
 
         });
-        // we should append listItemParent to all the 4 list items .appendChild(listItemParent)
-        const textContent = document.createTextNode(`${testObj.mainmenu}`);
-        // textContent.appendChild(listItemParent)
-        pop_up.appendChild(textContent)
-        if (testObj.subMenu.length===0){
-          
-        }else{
-          pop_up.appendChild(icon);
+
+  
+
+        // creating Unordered list
+
+        let count_li = 0;
+        while (count_li <= testObj["subMenu"].length - 1) {
+          testObj["subMenu"].forEach((x) => {
+            let listItemChild = document.createElement("li");
+            listItemChild.innerHTML = `${Object.keys(x)[0]}`;
+            count_li++;
+            listContainer.appendChild(listItemChild);
+          });
         }
-        pop_up.appendChild(listContainer)
+
+        const textContent = document.createTextNode(`${testObj.mainmenu}`);
+        anchorTag.appendChild(textContent);
+        pop_up.appendChild(anchorTag);
+
+        if (testObj.subMenu.length === 0) {
+        } else {
+          anchorTag.appendChild(icon);
+        }
+        pop_up.appendChild(listContainer);
         parent.appendChild(pop_up);
       });
     });
 };
-
-// All Functions
-// const arrowDown = (event) => {
-//   console.log("OK", event.target.id);
-// };
-
 getData();
