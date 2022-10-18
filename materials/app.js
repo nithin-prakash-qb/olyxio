c5652c9ba313a471d2dd32160c5bb5597921ec01
nithin.prakash <nithin.prakash@qburst.com>
2022-10-18 17:23:32 +0530

    started doing html and css of the location page

--- materials/app.js
+++ materials/app.js
@@ -46,66 +46,67 @@ const getData = () => {
         let icon = document.createElement("i");
         icon.setAttribute(
           "class",
-          "fa-solid fa-circle-chevron-down arrowDownIcon"
+          "fa-solid fa-circle-chevron-down arrowDownIcon  "
         );
         // icon.setAttribute("id", `${testObj.mainmenu}`);
         // icon.setAttribute("class","arrowDownIcon");
 
         let listContainer = document.createElement("ul");
         listContainer.className = "sub_li hiddens";
-        listContainer.dataset.menuId = testObj.mainmenu
+        listContainer.dataset.menuId = testObj.mainmenu;
 
         let anchorTag = document.createElement("a");
         anchorTag.className = "myAnchor";
 
-
         // loading subli for each main li
-        // anchorTag.addEventListener("click", (event) => {
-        //   console.log(event.target);
-        //   let dropDown = anchorTag.nextElementSibling;
-          // if (Object.values(dropDown.classList).includes("hiddens")) {
-          //   dropDown.classList.remove("hiddens");
-          // } else {
-          //   dropDown.classList.add("hiddens");
-          // }
-        // });
-
-        // Trying out 
         anchorTag.addEventListener("click", (event) => {
           // console.log(event.target);
-          let clicked = event.target
-          console.log(event.target)
-          let dropDown = clicked.nextElementSibling;
-          console.log(dropDown)
+
+          let dropDown = anchorTag.nextElementSibling;
           if (Object.values(dropDown.classList).includes("hiddens")) {
             dropDown.classList.remove("hiddens");
+            event.target.style.color = "#e62552";
           } else {
             dropDown.classList.add("hiddens");
+            event.target.style.color = "#0d093e";
           }
-
-          // let all_anchor_tag=document.querySelectorAll(".myAnchor")
-         
-          // all_anchor_tag.forEach((y)=>{
-          //   let dropDown = y.nextElementSibling;
-          //   if (Object.values(dropDown.classList).includes("hiddens")) {
-          //     dropDown.classList.remove("hiddens");
-          //   }else{
-          //     dropDown.classList.add("hiddens");
-          //   }
-
-          //   }
-          // )
-          // let dropDown = anchorTag.nextElementSibling;
-
-          // if (Object.values(dropDown.classList).includes("hiddens")) {
-          //   dropDown.classList.remove("hiddens");
-          // } else {
-          //   dropDown.classList.add("hiddens");
-          // }
-
         });
 
-  
+        // Trying out
+
+        // anchorTag.addEventListener("click", (event) => {
+        //   // console.log(event.target);
+        //   let clicked = event.target
+        //   console.log(event.target)
+        //   let dropDown = clicked.nextElementSibling;
+        //   console.log(dropDown)
+        //   if (Object.values(dropDown.classList).includes("hiddens")) {
+        //     dropDown.classList.remove("hiddens");
+        //   } else {
+        //     dropDown.classList.add("hiddens");
+        //   }
+
+        //   let all_anchor_tag=document.querySelectorAll(".myAnchor")
+
+        //   all_anchor_tag.forEach((y)=>{
+        //     let dropDown = y.nextElementSibling;
+        //     if (Object.values(dropDown.classList).includes("hiddens")) {
+        //       dropDown.classList.remove("hiddens");
+        //     }else{
+        //       dropDown.classList.add("hiddens");
+        //     }
+
+        //     }
+        //   )
+        //   let dropDown = anchorTag.nextElementSibling;
+
+        //   if (Object.values(dropDown.classList).includes("hiddens")) {
+        //     dropDown.classList.remove("hiddens");
+        //   } else {
+        //     dropDown.classList.add("hiddens");
+        //   }
+
+        // });
 
         // creating Unordered list
 
@@ -113,7 +114,10 @@ const getData = () => {
         while (count_li <= testObj["subMenu"].length - 1) {
           testObj["subMenu"].forEach((x) => {
             let listItemChild = document.createElement("li");
-            listItemChild.innerHTML = `${Object.keys(x)[0]}`;
+            let listItemAnchor = document.createElement("a");
+            listItemAnchor.innerHTML = `${Object.keys(x)[0]}`;
+            listItemAnchor.href = Object.values(x)[0];
+            listItemChild.appendChild(listItemAnchor);
             count_li++;
             listContainer.appendChild(listItemChild);
           });
@@ -133,3 +137,30 @@ const getData = () => {
     });
 };
 getData();
+
+// JS for location page
+const getDataLocation = () => {
+  fetch("location.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      header_fn(data)
+    });//closing of then
+  
+};
+getDataLocation();
+
+// function for getting the header
+let header_fn = (data)=>{
+  let main = document.getElementById("main");
+  let heading_un_li = document.createElement("ul");
+  data.forEach((testObj) => {
+    let heading_list_item = document.createElement("li");
+    let heading_button = document.createElement("button");
+    heading_button.innerHTML=`${testObj.country}`;
+    heading_list_item.appendChild(heading_button)
+    heading_un_li.appendChild(heading_list_item)
+  });
+  main.appendChild(heading_un_li)
+}
